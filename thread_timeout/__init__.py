2939d4e0e453f743973f16f3b87049001e1e5b5e
George Shuklin <george.shuklin@gmail.com>
2015-09-03 21:43:06 +0300

    pep8'ing

--- thread_timeout/__init__.py
+++ thread_timeout/__init__.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # (c) George Shuklin, 2015
-# 
+#
 # This library is free software; you can redistribute it and/or
 # Modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # Version 2.1 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # But WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 import threading
 import time
@@ -21,7 +21,7 @@ import signal
 import ctypes
 
 '''
-    thread_timeout allows to run piece of the python code safely regardless 
+    thread_timeout allows to run piece of the python code safely regardless
     of TASK_UNINTERRUPTIBLE issues.
 
     It provides single decorator, adding a timeout for the function call.
@@ -40,31 +40,35 @@ import ctypes
             print ("NFS seems to be hung")
 
 
-    thread_timeout works by running specified function in separate thread and waiting
-    for timeout (or finalization) of the thread to return value or raise Exception.
-    If thread is not finished before timeout, thread_timeout may try to terminate
-    thread according to kill_wait value (see below).
+    thread_timeout works by running specified function in separate
+    thread and waiting for timeout (or finalization) of the thread
+    to return value or raise exception.
+    If thread is not finished before timeout, thread_timeout will
+    try to terminate thread according to kill value (see below).
 
     thread_timeout(timeout, kill=True, kill_wait=0.1)
 
     timeout - seconds, floating, how long to wait thread.
     kill - if True (default) attempt to terminate thread with function
-    kill_wait - how long to wait after killing before reporting an unresponsive thread 
-    
+    kill_wait - how long to wait after killing before reporting
+    an unresponsive thread
+
     THREAD KILLING
 
-    Thread killing implemented on python level: it will terminate python code, but will not terminate any IO operations or subprocess calls.
+    Thread killing implemented on python level: it will terminate python
+    code, but will not terminate any IO operations or subprocess calls.
 
     Exceptions:
 
-    ExecTimeoutException - function did not finish on time, timeout (base class for all following exceptions)
-    KilledExecTimeoutException - there was a timeout and thread with function was killed successfully
-    FailedKillExecTimeoutException - there was a timeout and kill attempt but the thread refuses to die
-    NotKillExecTimeoutException - there was a timeout and there was no attempt to kill thread
+    ExecTimeoutException - function did not finish on time, timeout
+        (base class for all following exceptions)
+    KilledExecTimeoutException - there was a timeout and thread
+        with function was killed successfully
+    FailedKillExecTimeoutException - there was a timeout and kill attempt
+        but the thread refuses to die
+    NotKillExecTimeoutException - there was a timeout and there
+        was no attempt to kill thread
 '''
-
-
-
 
 
 def _kill_thread(t):
@@ -72,20 +76,25 @@ def _kill_thread(t):
     # by Johan Dahlin
     # rewrited to avoid licence uncertainty
     exc = ctypes.py_object(SystemExit)
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(t.ident), exc)
+    ct = ctypes.c_long(t.ident)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ct, exc)
     if res == 0:
         raise ValueError("nonexistent thread id")
     else:
         ctypes.pythonapi.PyThreadState_SetAsyncExc(t.ident, None)
 
+
 class ExecTimeoutException(BaseException):
     pass
+
 
 class KilledExecTimeoutException(ExecTimeoutException):
     pass
 
+
 class FailedKillExecTimeoutException(ExecTimeoutException):
     pass
+
 
 class NotKillExecTimeoutException(ExecTimeoutException):
     pass
@@ -93,8 +102,10 @@ class NotKillExecTimeoutException(ExecTimeoutException):
 
 def thread_timeout(delay, kill=True, kill_wait=0.1):
     def generate_decorator(function):
-        def decorator (*args, **kwargs):
+
+        def decorator(*args, **kwargs):
             result = [None]
+
             def inner_worker():
                 result[0] = function(*args, **kwargs)
             thread = threading.Thread(target=inner_worker)
@@ -103,19 +114,24 @@ def thread_timeout(delay, kill=True, kill_wait=0.1):
             thread.join(delay)
             if thread.isAlive():
                     if not kill:
-                        raise NotKillExecTimeoutException("Timeout and no kill attempt")
+                        raise NotKillExecTimeoutException(
+                            "Timeout and no kill attempt")
                     _kill_thread(thread)
                     time.sleep(kill_wait)
                     if thread.isAlive():
-                        raise FailedKillExecTimeoutException("Timeout, thread refuses to die in %s seconds" % kill_wait)
+                        raise FailedKillExecTimeoutException(
+                            "Timeout, thread refuses to die in %s seconds" %
+                            kill_wait)
                     else:
-                        raise KilledExecTimeoutException("Timeout and thread was killed")
+                        raise KilledExecTimeoutException(
+                            "Timeout and thread was killed")
             return result[0]
         return decorator
     return generate_decorator
 
 if __name__ == "__main__":
     print ("Running tests")
+
     @thread_timeout(2)
     def test1(delay):
         print ("  .. sleeing for %s" % delay)
@@ -126,7 +142,6 @@ if __name__ == "__main__":
         print ("Test1 OK")
     except ExecTimeoutException:
         print ("Test1 failed, timeout too soon")
-    
 
     @thread_timeout(1)
     def test2(delay):
@@ -138,7 +153,7 @@ if __name__ == "__main__":
         test2(3)
         raise Exception("Test2 failed: timeout does not work")
     except ExecTimeoutException as e:
-        print ("  .. got excepted execption %s"%repr(e))
+        print ("  .. got excepted execption %s" % repr(e))
         print("Test2 OK")
 
     @thread_timeout(1)
@@ -152,7 +167,7 @@ if __name__ == "__main__":
 
     @thread_timeout(1)
     def test4(x):
-        for a in range(0,x):
+        for a in range(0, x):
             time.sleep(2)
     try:
         test4(2)
@@ -161,10 +176,9 @@ if __name__ == "__main__":
 
     @thread_timeout(1, kill=False)
     def test5(x):
-        for a in range(0,x):
+        for a in range(0, x):
             time.sleep(2)
     try:
         test5(2)
     except NotKillExecTimeoutException as e:
         print ("Test5 OK, got expected exception %s" % repr(e))
-    
