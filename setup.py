cb9d15ec1a2ca4be0fa8a1d2a5c63785bae45f12
George Shuklin <george.shuklin@gmail.com>
2015-09-07 18:31:37 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(name='thread_timeout',
+          version='1.0',
+          description='''Decorator to execute functionin in
+            separate thread with timeout''',
+          author='George Shuklin',
+          author_email='george.shuklin@gmail.com',
+          url='https://github.com/amarao/thread_timeout',
+          packages=['Queue','wrapt','ctypes'],
+          license='LGPL',
+          classifiers=[
+                       "Development Status :: 4 - Beta",
+                       "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+                       "Topic :: Software Development :: Libraries"
+          ],
+          )
