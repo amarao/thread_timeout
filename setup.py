d3bd8525e0fc4434add7c553a5952f079f4c0e9a
George Shuklin <george.shuklin@gmail.com>
2015-09-08 15:58:33 +0300

    No '__main__' in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
 
-if __name__ == '__main__':
-    setup(name='thread_timeout',
-          version='1.0',
-          description='''Decorator to execute functionin in
-            separate thread with timeout''',
-          author='George Shuklin',
-          author_email='george.shuklin@gmail.com',
-          url='https://github.com/amarao/thread_timeout',
-          packages=find_packages(),
-          install_requires=['wrapt'],
-          license='LGPL',
-          classifiers=[
-                       "Development Status :: 4 - Beta",
-                       "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-                       "Topic :: Software Development :: Libraries"
-          ],
-          )
+setup(name='thread_timeout',
+      version='1.0',
+      description='''Decorator to execute functionin in
+        separate thread with timeout''',
+      author='George Shuklin',
+      author_email='george.shuklin@gmail.com',
+      url='https://github.com/amarao/thread_timeout',
+      packages=find_packages(),
+      install_requires=['wrapt'],
+      license='LGPL',
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+                   "Topic :: Software Development :: Libraries"
+      ],
+      )
