c0b6e0af9a1ef1cff146c2ee955fc4f7d17ccc65
George Shuklin <george.shuklin@gmail.com>
2015-10-10 20:44:51 +0300

    Debian part

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+class PyTest(Command):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        print "foo"
+        import subprocess
+        import pytest
+        pytest.main('run_test.py')
+#        subprocess.check_call(['py.test'])
 
 setup(name='thread_timeout',
       version='1.0',
@@ -10,6 +22,8 @@ setup(name='thread_timeout',
       url='https://github.com/amarao/thread_timeout',
       packages=find_packages(),
       install_requires=['wrapt'],
+      cmdclass = {'test': PyTest},
+
       license='LGPL',
       classifiers=[
                    "Development Status :: 4 - Beta",
